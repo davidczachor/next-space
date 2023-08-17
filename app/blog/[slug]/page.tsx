d29e8723230332bf7695159c11e2658f225d5b32
davidczachor <david@czachor.dev>
2023-08-16 23:10:13 -0400

    Static Generation

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 3600
+
 interface Post {
     title: string
     slug: string
@@ -6,6 +8,15 @@ interface Post {
 
 interface Props {
     params: { slug: string }
+}
+
+export async function generateStaticParams() {
+    const posts: Post[] = await fetch('http://localhost:3000/api/content').then(
+        (res) => res.json()
+    )
+    return posts.map((post) => ({
+        params: { slug: post.slug },
+    }))
 }
 
 export default async function BlogPostPage({ params }: Props) {
